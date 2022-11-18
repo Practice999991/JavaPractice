d6f626e3da539be025152a2f4da8d5ccf9b3836e
gBau99 <gideonbauer999@gmail.com>
2022-11-18 14:15:03 -0500

    Yes3

---
+++ mainContainer/addends.java
@@ -0,0 +1,6 @@
+package mainContainer;
+
+public class addends {
+    public double first = 2;
+    public double second = 1;
+}cd 
---
+++ mainContainer/commandCode.java
@@ -0,0 +1,6 @@
+
+
+public class commandCode {
+    public double first = 2;
+    public double second = 1;
+}
