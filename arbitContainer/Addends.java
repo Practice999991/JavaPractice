059078181b33a387ee543ed638311d0c25e4c39b
gBau99 <gideonbauer999@gmail.com>
2022-11-19 21:02:09 -0500

    new structure

--- arbitContainer/Addends.java
+++
@@ -1,6 +0,0 @@
-package arbitContainer;
-
-public class Addends {
-  public static double first = 3;
-  public static double second = 4;
-}
---
+++ myPackage/Addends.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class Addends {
+  public double first = 0.0;
+  public double second = 0.0;
+
+  public Addends() {
+    first = 3.0;
+    second = 4.0;
+  }
+
+  public Addends(double f, double s) {
+    first = f;
+    second = s;
+  }
+}
